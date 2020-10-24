aeff461b5d597952b402213f07991b887e4f0c0c
Hacke Wayne <hacklew@hotmail.com>
2020-10-24 18:03:12 +1300

    dangle

---
+++ src/dangle.spec.ts
@@ -0,0 +1,13 @@
+import { matchResult } from "./dangle";
+
+describe('match result', () => {
+    [
+        { myScore: 10, opponentScore: 20, result: 'Loss' },
+        { myScore: 20, opponentScore: 10, result: 'Win' },
+        { myScore: 20, opponentScore: 20, result: 'Tie' },
+    ].forEach(testcase => it(testcase.result, () => {
+        const actual = matchResult(testcase.myScore, testcase.opponentScore);
+
+        expect(actual).toEqual(testcase.result);
+    }));
+});
