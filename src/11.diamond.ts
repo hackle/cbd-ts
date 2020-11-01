d532011c83b82756281ad28d635bc909692d9f59
Hacke Wayne <hacklew@hotmail.com>
2020-11-01 18:08:21 +1300

    diamond

--- src/11.diamond.ts
+++ src/11.diamond.ts
@@ -1,12 +1,12 @@
 /*
     given any letter, build a diamond shape.
     For example, with D
-    ---D---
-    --C-C--
-    -B---B-
-    A-----A
-    -B---B-
-    --C-C--
+    ---A---
+    --B-B--
+    -C---C-
+    D-----D
+    -C---C-
+    --B-B--
     ---A---
 */
 export function diamond(letter: string): string {
